4f7777537ceb6bd0f5122db404aa76d3e290fcbb
Ganapaved <vedvedanmoolya@gmail.com>
2025-07-16 19:25:21 +0530

    added .env to replace with data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
-
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 
@@ -24,7 +24,9 @@ const menuRoutes = require('./routes/menu_routes')
 app.use('/menu' , menuRoutes)
 
 
-app.listen(3000 , () =>{
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , () =>{
     console.log('listening in port ');
     
 })
